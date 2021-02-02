a127f0acfe4be8874031b681f35c2196694a83cc
Carlos Alberto Duarte Pinto <alberto.duarte@eb.mil.br>
2021-02-02 15:22:50 -0300

    Add csv functionality (incomplete)

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,8 @@
 import os
+from datetime import datetime
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
+from ptax_client import PTAXClient
 
 
 application = Flask(__name__)
@@ -8,7 +10,31 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return jsonify(message='Hi there')
+    return jsonify(todo='Documentation')
+
+
+@application.route('/ptax_data')
+def get_ptax_data():
+    c = PTAXClient()
+    parameters = ('start', 'end', 'currency')
+
+    if any(parameter not in request.args for parameter in parameters):
+        return jsonify(
+            message='Required parameters: '
+            + ', '.join(parameters[:-1]) + ' and ' + parameters[-1]
+        )
+
+    data_frame = c.get(
+        start_date=datetime.strptime(request.args.get('start'), '%d/%m/%Y'),
+        end_date=datetime.strptime(request.args.get('end'), '%d/%m/%Y'),
+        currency=request.args.get('currency')
+    )
+
+    response = make_response(data_frame.to_csv())
+    response.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    response.headers["Content-Type"] = "text/csv"
+
+    return response
 
 
 if __name__ == '__main__':
